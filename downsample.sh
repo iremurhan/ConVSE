516fd0c7986d13cadab661b30d97fcb4057b6da7
Irem Urhan <biremurhan@gmail.com>
2024-10-07 14:30:49 +0300

    Created a shell script to run data_downsampler.py.

---
+++ downsample.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --container-image ghcr.io\#bouncmpe/cuda-python3
+#SBATCH --gpus=1
+#SBATCH --cpus-per-gpu=8
+#SBATCH --mem-per-gpu=40G
+#SBATCH --time=10000
+
+# Activate Python virtual environment
+source /opt/python3/venv/base/bin/activate
+
+# Define environment variables
+DATA_PATH="data/coco_precomp"  # Path to original dataset directory
+OUTPUT_PATH="data/coco_precomp_small"  # Path to save subsampled dataset
+NUM_SAMPLES=1000  # Number of samples to take from each dataset (e.g., train, dev, test)
+
+# Run the downsampling script
+python3 subsample_data.py --input_dir "$DATA_PATH" --output_dir "$OUTPUT_PATH" --num_samples "$NUM_SAMPLES"
